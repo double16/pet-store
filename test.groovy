1b4a1aba787ff46151fae470cee9df240389490b
Patrick Double <pat@patdouble.com>
2015-04-14 15:19:42 -0500

    Use httpclient directly for upload

--- test.groovy
+++ test.groovy
@@ -1,4 +1,5 @@
 #!/usr/bin/env groovy
+@GrabResolver(name='local', root='/Users/pdouble/.m2/repository')
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.2')
 // This brings in slf4j, log4j12 (as dependancies) and the bridge to log4j
 @Grab(group='org.slf4j', module='slf4j-log4j12', version='1.7.7')
@@ -13,6 +14,9 @@ import groovy.util.slurpersupport.GPathResult
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import static groovyx.net.http.ContentType.URLENC
+import org.apache.http.impl.client.DefaultHttpClient
+import org.apache.http.client.methods.HttpPut
+import org.apache.http.entity.FileEntity
 
 Logger.getLogger("org.apache.http.headers").setLevel(Level.DEBUG)
 Logger.getLogger("org.apache.http.wire").setLevel(Level.DEBUG)
@@ -26,15 +30,17 @@ def file = new File("build/distributions/pet-store-slug-0.1.tgz")
         // create the slug
         def slug = heroku.post(path: 'slugs', requestContentType: "application/json",
                 body: ["process_types": [ "web": "java -jar \$JAVA_OPTS pet-store-0.1.jar"]])
+	System.err.println "Created slug ${slug.data.id}"
 
         // upload the slug
-        new HTTPBuilder().request(slug.data.blob.url, Method.PUT, ContentType.ANY) {
-            requestContentType = 'application/octet-stream'
-            body = file.newInputStream()
-            headers.'Content-Type' = ''
+	System.err.println "Uploading ${file} to ${slug.data.blob.url}"
+        def res = new DefaultHttpClient().execute(new HttpPut(URI: slug.data.blob.url, entity: new FileEntity(file)))
+        if (res.statusLine.statusCode > 399) {
+            throw new IOException(re.statusLine.reasonPhrase)
         }
 
         // release the slug
+	System.err.println "Releasing slug ${slug.data.id}"
         heroku.post(path: 'releases', requestContentType: 'application/json',
             body:[ 'slug': slug.data.id ])
 
