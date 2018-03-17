99728bfae09eeb7363245b3aa7004d123d17bb32
Patrick Double <pat@patdouble.com>
2018-03-16 21:50:13 -0500

    Fix load balancer

--- src/main/tf/run/application.tf
+++ src/main/tf/run/application.tf
@@ -23,6 +23,31 @@ resource "aws_security_group" "web_internal" {
   }
 }
 
+resource "aws_security_group" "web_external" {
+  name        = "${var.application_name}-web-external"
+  description = "Web server exposed to the Internet"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    "Application" = "${var.application_name}"
+    "Environment" = "${terraform.workspace}"
+  }
+}
+
 resource "aws_cloudwatch_log_group" "app" {
   name = "/ecs/${var.application_name}"
   retention_in_days = "30"
@@ -60,6 +85,35 @@ resource "aws_iam_policy" "app_run" {
               "logs:CreateLogStream",
               "logs:PutLogEvents"
           ]
+      },
+      {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AttachNetworkInterface",
+                "ec2:CreateNetworkInterface",
+                "ec2:CreateNetworkInterfacePermission",
+                "ec2:DeleteNetworkInterface",
+                "ec2:DeleteNetworkInterfacePermission",
+                "ec2:Describe*",
+                "ec2:DetachNetworkInterface",
+                "ec2:AuthorizeSecurityGroupIngress",
+                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                "elasticloadbalancing:DeregisterTargets",
+                "elasticloadbalancing:Describe*",
+                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                "elasticloadbalancing:RegisterTargets",
+                "route53:ChangeResourceRecordSets",
+                "route53:CreateHealthCheck",
+                "route53:DeleteHealthCheck",
+                "route53:Get*",
+                "route53:List*",
+                "route53:UpdateHealthCheck",
+                "servicediscovery:DeregisterInstance",
+                "servicediscovery:Get*",
+                "servicediscovery:List*",
+                "servicediscovery:RegisterInstance"
+            ],
+            "Resource": "*"
       }
     ]
 }
@@ -76,6 +130,13 @@ resource "aws_iam_role" "app_run" {
       "Effect": "Allow",
       "Principal": {
         "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
     }
@@ -111,5 +172,44 @@ resource "aws_ecs_service" "app" {
   network_configuration {
     subnets = [ "${aws_subnet.private1.id}", "${aws_subnet.private2.id}" ]
     security_groups = [ "${aws_security_group.web_internal.id}" ]
+  }
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.app.arn}"
+    container_name = "app"
+    container_port = 8080
+  }
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "${var.application_name}-${terraform.workspace}-lb-http-group"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_vpc.main.id}"
+}
+
+resource "aws_lb" "app" {
+  name = "${var.application_name}-${terraform.workspace}"
+  load_balancer_type = "application"
+  internal = false
+  subnets = [ "${aws_subnet.private1.id}", "${aws_subnet.private2.id}" ]
+  security_groups = [ "${aws_security_group.web_external.id}" ]
+
+  tags {
+    "Application" = "${var.application_name}"
+    "Environment" = "${terraform.workspace}"
+  }
+
+  depends_on = [ "aws_iam_policy.app_run" ]
+}
+
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = "${aws_lb.app.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.app.arn}"
+    type             = "forward"
   }
 }
