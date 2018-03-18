ea43c8baf69450a1252d7b27dc517fafed22dbe6
Patrick Double <pat@patdouble.com>
2018-03-17 22:48:37 -0500

    TF organization and some outputs

---
+++ src/main/tf/pipeline/iam.tf
@@ -0,0 +1,173 @@
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-role-${var.application_name}"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "codebuild_policy" {
+  name = "codebuild-policy-${var.application_name}"
+  path = "/service-role/"
+  description = "Policy used in trust relationship with CodeBuild"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Resource": [
+        "*"
+      ],
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+          "s3:PutObject",
+          "s3:GetObject"
+      ],
+      "Resource": [
+          "${aws_s3_bucket.codebuild_bucket.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codecommit:GetBranch",
+        "codecommit:GetCommit",
+        "codecommit:UploadArchive",
+        "codecommit:GetUploadArchiveStatus",
+        "codecommit:CancelUploadArchive"
+      ],
+      "Resource": "${aws_codecommit_repository.application.arn}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+     },
+     {
+      "Action": [
+          "ecr:GetAuthorizationToken"
+      ],
+      "Resource": "*",
+      "Effect": "Allow"
+     },
+     {
+      "Action": [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+      ],
+      "Resource": "${aws_ecr_repository.app_repo.arn}",
+      "Effect": "Allow"
+     },
+     {
+      "Action": [
+        "ecs:DescribeServices",
+        "ecs:DescribeTaskDefinition",
+        "ecs:DescribeTasks",
+        "ecs:ListTasks",
+        "ecs:RegisterTaskDefinition",
+        "ecs:UpdateService",
+        "iam:PassRole"
+      ],
+      "Resource": "*",
+      "Effect": "Allow"
+     }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_policy_attachment" "codebuild_policy_attachment" {
+  name = "codebuild-policy-attachment-${var.application_name}"
+  policy_arn = "${aws_iam_policy.codebuild_policy.arn}"
+  roles = [
+    "${aws_iam_role.codebuild_role.id}"
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy_attachment_ecs_poweruser" {
+  role = "${aws_iam_role.codebuild_role.id}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
+}
+
+resource "aws_iam_role" "oncommit_role" {
+  name = "${var.application_name}-${terraform.workspace}-oncommit"
+  path = "/service-role/"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "oncommit_policy" {
+  name = "start-pipeline-execution-${var.application_name}-${terraform.workspace}"
+  path = "/service-role/"
+  description = "Policy used in trust relationship with CloudWatch"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "codepipeline:StartPipelineExecution"
+            ],
+            "Resource": [
+                "${aws_codepipeline.application.arn}"
+            ]
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_policy_attachment" "oncommit_policy_attachment" {
+  name = "${var.application_name}-${terraform.workspace}-oncommit-policy-attachment"
+  policy_arn = "${aws_iam_policy.oncommit_policy.arn}"
+  roles = [
+    "${aws_iam_role.oncommit_role.id}"
+  ]
+}
